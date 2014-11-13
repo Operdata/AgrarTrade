5caa2a3411365a289122e1166267109e68804631
Ricardo Torre <rthinf@gmail.com>
2014-11-13 11:31:29 +0100

    AgrarTrade v0.0.1 built000005
    Añadida página sobre nosotros (incluido menu).
    Pequeñas actualizaciones.

--- pages/sobre_nosotros_page.php
+++ pages/sobre_nosotros_page.php
@@ -7,29 +7,29 @@
  */
 
 ?>
-<!doctype html>
+
 <html lang='es'>
 <head>
    <meta charset='utf-8'>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="css/menu/styles.css">
+   <link rel="stylesheet" href="../css/menu/styles.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-   <script src="js/scripst/menu_script.js"></script>
+   <script src="../js/scripst/menu_script.js"></script>
    <title>AgrarTrade</title>
 </head>
 <body>
 
 <div id='cssmenu'>
-<ul>
-   <li class='active'><a href='#'>Home</a></li>
-   <li><a href='#'>Tierras en venta</a></li>
-   <li><a href='#'>Subastas de tierras</a></li>
-   <li><a href='#'>Venta de Maquinaria</a></li>
-   <li><a href='#'>Alquiler de Maquinaria</a></li>
-   <li><a href='#'>Contratistas</a></li>
-   <li><a href='pages/sobre_nosotros_page.php'>Sobre Nosotros</a></li>
-</ul>
+    <ul>
+        <li class='active'><a href='../index.php'>Home</a></li>
+       <li><a href='#'>Tierras en venta</a></li>
+       <li><a href='#'>Subastas de tierras</a></li>
+       <li><a href='#'>Venta de Maquinaria</a></li>
+       <li><a href='#'>Alquiler de Maquinaria</a></li>
+       <li><a href='#'>Contratistas</a></li>
+       <li><a href='#'>Sobre Nosotros</a></li>
+    </ul>
 </div>
 
 </body>
