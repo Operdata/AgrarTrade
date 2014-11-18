1134df8273e74719314cc5aa5e776395d1ba298b
Ricardo Torre <rthinf@gmail.com>
2014-11-18 17:29:30 +0100

    AgrarTrade v0.0.1 built000007
    Adicción de carpetas

--- index.php
+++ index.php
@@ -6,16 +6,19 @@
  * and open the template in the editor.
  */
 
+
 ?>
 <!doctype html>
 <html lang='es'>
 <head>
+    
    <meta charset='utf-8'>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="css/menu/styles.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="js/scripst/menu_script.js"></script>
+   
    <title>AgrarTrade</title>
 </head>
 <body>
